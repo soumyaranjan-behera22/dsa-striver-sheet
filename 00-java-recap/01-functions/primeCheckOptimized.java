554209a3204a8769d9b45eabf1a509ca3b4330d7
Soumyaranjan Behera <soumya.rbehera007@gmail.com>
2026-03-16 00:44:29 +0530

    modify code

---
+++ 00-java-recap/01-functions/primeCheckOptimized.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class prime_check_optimize {
+    public static boolean isPrime(int n){
+        boolean check = true;
+        for(int i =2;i*i<n;i++){
+            if(n%i == 0){
+                return false;
+            }
+            return true;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+       boolean res= isPrime(n);
+       System.out.println(res);
+    }
+}
